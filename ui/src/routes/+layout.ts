8c0e81dffac5b51e4a369fc55cabb7d7d2eacb4c
boris <boris.lok@cienet.com>
2024-09-09 09:07:27 +0800

    [Feature] Support i18n.

---
+++ ui/src/routes/+layout.ts
@@ -0,0 +1,12 @@
+// +layout.ts
+import {browser} from '$app/environment'
+import '$lib/i18n' // Import to initialize. Important :)
+import {locale, waitLocale} from 'svelte-i18n'
+
+/** @type {import('./$types').LayoutServerLoad} */
+export async function load() {
+    if (browser) {
+        locale.set(window.navigator.language);
+    }
+    await waitLocale();
+}
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import {t, locale} from 'svelte-i18n';
+
+    // Optionally, you can set the locale manually
+    // $locale = 'en'; // For English
+    $locale = 'zh'; // For Spanish
+</script>
+
+<h1>{$t('welcome')} to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>

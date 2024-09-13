44094d213f7e41fdbb9d2ffe999212c77705bc1a
boris <boris.lok@cienet.com>
2024-09-13 17:19:51 +0800

    [Feature] Impl `delete_member` API.

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -1,4 +1,5 @@
 pub use create_member::create_member;
+pub use delete_member::delete_member;
 pub use get_member::get_member;
 pub use health::health_check;
 pub use upload_member_image::upload_member_image;
@@ -7,4 +8,5 @@ mod create_member;
 mod health;
 mod upload_member_image;
 
+mod delete_member;
 mod get_member;
--- backend/src/startup.rs
+++ backend/src/startup.rs
@@ -1,7 +1,7 @@
-use crate::api::{create_member, get_member, health_check, upload_member_image};
+use crate::api::{create_member, delete_member, get_member, health_check, upload_member_image};
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::utils::image::ImageUtil;
-use axum::routing::{get, post};
+use axum::routing::{delete, get, post};
 use axum::{Extension, Router};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
@@ -22,7 +22,8 @@ pub async fn run(config: Settings, listener: TcpListener) -> Result<(), std::io:
     let member_routes = Router::new()
         .route("/members", post(create_member))
         .route("/members/:id/avatar", post(upload_member_image))
-        .route("/members/:lang/:id", get(get_member));
+        .route("/members/:lang/:id", get(get_member))
+        .route("/members/:id", delete(delete_member));
 
     let admin_routes = Router::new().merge(member_routes);
 

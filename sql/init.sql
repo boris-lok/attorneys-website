238b825a6106ac12f12e0d214781fc64eac6082f
boris <boris.lok@cienet.com>
2024-09-25 11:30:49 +0800

    [Feature] Add `db` in docker-compose.yml

---
+++ sql/init.sql
@@ -0,0 +1,45 @@
+-- Add up migration script here
+
+create table member
+(
+    id         varchar(32) not null,
+    created_at timestamptz not null,
+    deleted_at timestamptz,
+    seq        smallint    not null,
+    primary key (id)
+);
+
+create table content
+(
+    id         varchar(32) not null,
+    data       jsonb       not null,
+    created_at timestamptz not null,
+    updated_at timestamptz,
+    language   char(4)     not null,
+    primary key (id, language)
+);
+
+create table avatar
+(
+    id   varchar(32) not null,
+    data jsonb       not null,
+    primary key (id)
+);
+
+create table service
+(
+    id         varchar(32) not null,
+    created_at timestamptz not null,
+    deleted_at timestamptz,
+    seq        smallint    not null,
+    primary key (id)
+);
+
+create table home
+(
+    id         varchar(32) not null,
+    created_at timestamptz not null,
+    deleted_at timestamptz,
+    seq        smallint    not null,
+    primary key (id)
+);

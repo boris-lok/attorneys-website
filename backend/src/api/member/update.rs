9d1419e8b3fc9ba7ebd991bb6964757b1924ee7f
boris <boris.lok@cienet.com>
2024-10-07 14:42:15 +0800

    [Feature] Impl `update resources` API.

--- backend/src/api/member/update.rs
+++ backend/src/api/member/update.rs
@@ -1,43 +1,43 @@
-// use crate::api::api_error::ApiError;
-// use crate::domain::member::update::Error;
-// use crate::startup::AppState;
-// use crate::uow::member::SqlxMemberUnitOfWork;
-// use axum::extract::State;
-// use axum::http::StatusCode;
-// use axum::Json;
-// use axum_extra::extract::WithRejection;
-// use serde::Deserialize;
-// use tokio::sync::Mutex;
-//
-// #[derive(Debug, Deserialize)]
-// pub(crate) struct UpdateMemberRequest {
-//     member_id: String,
-//     name: String,
-//     description: String,
-//     language: String,
-// }
-//
-// pub async fn update_member(
-//     State(state): State<AppState>,
-//     WithRejection(Json(req), _): WithRejection<Json<UpdateMemberRequest>, ApiError>,
-// ) -> Result<StatusCode, ApiError> {
-//     let req = crate::domain::member::update::Request {
-//         member_id: req.member_id,
-//         data: crate::domain::member::entities::Data {
-//             name: req.name,
-//             description: req.description,
-//         },
-//         language: req.language,
-//     };
-//
-//     let uow = SqlxMemberUnitOfWork::new(&state.pool)
-//         .await
-//         .map_err(|e| ApiError::InternalServerError(e.to_string()))?;
-//     let uow = Mutex::new(uow);
-//
-//     match crate::domain::member::update::execute(uow, req).await {
-//         Ok(()) => Ok(StatusCode::OK),
-//         Err(Error::BadRequest) => Err(ApiError::BadRequest),
-//         Err(Error::Unknown(e)) => Err(ApiError::InternalServerError(e.to_string())),
-//     }
-// }
+use crate::api::api_error::ApiError;
+use crate::domain::entities::{MemberData, Resource};
+use crate::startup::AppState;
+use crate::uow::InDatabase;
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::Json;
+use axum_extra::extract::WithRejection;
+use serde::Deserialize;
+use tokio::sync::Mutex;
+
+#[derive(Debug, Deserialize)]
+pub(crate) struct UpdateMemberRequest {
+    id: String,
+    name: String,
+    description: String,
+    language: String,
+}
+
+pub async fn update_member(
+    State(state): State<AppState>,
+    WithRejection(Json(req), _): WithRejection<Json<UpdateMemberRequest>, ApiError>,
+) -> Result<StatusCode, ApiError> {
+    let req = crate::domain::resources::update::Request {
+        id: req.id,
+        data: Resource::Member(MemberData::new(req.name, req.description)),
+        language: req.language,
+    };
+
+    let uow = InDatabase::new(&state.pool)
+        .await
+        .map_err(|e| ApiError::InternalServerError(e.to_string()))?;
+    let uow = Mutex::new(uow);
+
+    match crate::domain::resources::update::execute(uow, req).await {
+        Ok(_) => Ok(StatusCode::OK),
+        Err(crate::domain::resources::update::Error::BadRequest) => Err(ApiError::BadRequest),
+        Err(crate::domain::resources::update::Error::NotFound) => Err(ApiError::NotFound),
+        Err(crate::domain::resources::update::Error::Unknown(e)) => {
+            Err(ApiError::InternalServerError(e.to_string()))
+        }
+    }
+}
--- backend/src/startup.rs
+++ backend/src/startup.rs
@@ -2,7 +2,7 @@ use crate::api::{
     create_article, create_contact, create_home, create_member, create_service, delete_article,
     delete_member, delete_service, health_check, list_articles, list_contact, list_home,
     list_members, list_services, retrieve_article, retrieve_contact, retrieve_home,
-    retrieve_member, retrieve_service, upload_member_avatar,
+    retrieve_member, retrieve_service, update_member, upload_member_avatar,
 };
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::utils::image::ImageUtil;
@@ -27,7 +27,7 @@ pub async fn run(config: Settings, listener: TcpListener) -> Result<(), std::io:
 
     // Config the routes
     let admin_member_routes = Router::new()
-        .route("/members", post(create_member))
+        .route("/members", post(create_member).put(update_member))
         .route("/members/:id", delete(delete_member))
         .route("/members/:id/avatar", post(upload_member_avatar));
     let member_routes = Router::new()

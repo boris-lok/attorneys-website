6bbf728cc9f3a0a1ab6836de60635d19063169be
boris <boris.lok@cienet.com>
2024-10-11 11:39:26 +0800

    [Refactoring] fetch the information from API in contact page.

--- ui/src/routes/(app)/contact/+page.svelte
+++ ui/src/routes/(app)/contact/+page.svelte
@@ -1,43 +1,76 @@
 <script lang="ts">
 	import { t } from 'svelte-i18n';
+	import { onMount } from 'svelte';
+	import { Contacts } from '$lib/services';
+	import type { Language } from '$lib/models/Language';
+	import { startWithTap } from '$lib/utils';
+	import { finalize, tap } from 'rxjs';
+	import Loading from '$lib/components/Loading.svelte';
 
 	const googleMapAPIKey = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
-	const coordinates = import.meta.env.VITE_COMPANY_CENTER_COORDINATES;
 	const placeID = import.meta.env.VITE_COMPANY_PLACE_ID;
-	let address = '100506臺北市中正區忠孝西路一段50號20樓之32（亞洲廣場大樓）';
-	let phone = '02-23707088';
-	let email = 'info@example.com';
+
+	// The address wants to show in the UI
+	let address = '';
+	// The phone number wants to show in the UI
+	let phone = '';
+	// The email address wants to show in the UI
+	let email = '';
+	// The loading indicator
+	let isLoading = false;
+	// The language is used to fetch the specified language resource
+	let language: Language = 'zh';
+
+	onMount(() => {
+		Contacts.list(language)
+			.pipe(
+				startWithTap(() => isLoading = true),
+				finalize(() => isLoading = false),
+				tap(e => {
+					if (e) {
+						address = e.data.address;
+						phone = e.data.phone;
+						email = e.data.email;
+					}
+				})
+			)
+			.subscribe();
+	});
 </script>
 
-<div class="company-info-section">
-	<div class="map-info">
-		<iframe
-			allowfullscreen
-			loading="lazy"
-			referrerpolicy="no-referrer-when-downgrade"
-			src="https://www.google.com/maps/embed/v1/place?key={googleMapAPIKey}
+{#if isLoading}
+	<Loading />
+{:else}
+	<div class="company-info-section">
+		<div class="map-info">
+			<iframe
+				allowfullscreen
+				loading="lazy"
+				referrerpolicy="no-referrer-when-downgrade"
+				src="https://www.google.com/maps/embed/v1/place?key={googleMapAPIKey}
 		&q={placeID}
 		&zoom=16
 "
-			style="border:0"
-			title="company map">
-		</iframe>
+				style="border:0"
+				title="company map">
+			</iframe>
+		</div>
+		<div class="contact-info">
+			<div class="block">
+				<h3>{$t('address')}</h3>
+				<p>{address}</p>
+			</div>
+			<div class="block">
+				<h3>{$t('phone')}</h3>
+				<p><a href="tel:{phone}">{phone}</a></p>
+			</div>
+			<div class="block">
+				<h3>{$t('email')}</h3>
+				<p><a href="mailto:{email}">{email}</a></p>
+			</div>
+		</div>
 	</div>
-	<div class="contact-info">
-		<div class="block">
-			<h3>{$t('address')}</h3>
-			<p>{address}</p>
-		</div>
-		<div class="block">
-			<h3>{$t('phone')}</h3>
-			<p><a href="tel:{phone}">{phone}</a></p>
-		</div>
-		<div class="block">
-			<h3>{$t('email')}</h3>
-			<p><a href="mailto:{email}">{email}</a></p>
-		</div>
-	</div>
-</div>
+{/if}
 
 <style lang="scss">
   .company-info-section {
@@ -48,6 +81,10 @@
       height: 320px;
       border: none;
     }
+
+		.contact-info {
+			width: 100%;
+		}
 
     .block {
       margin: 1rem 0;

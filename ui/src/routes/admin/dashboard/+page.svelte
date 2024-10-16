b489058f961f9757233cf18be2ae9d0336ae9438
boris <boris.lok@cienet.com>
2024-10-16 11:55:18 +0800

    [UI] modify the dashboard styles.

--- ui/src/routes/admin/dashboard/+page.svelte
+++ ui/src/routes/admin/dashboard/+page.svelte
@@ -6,18 +6,63 @@
 	import MemberManager from '$lib/components/dashboard/MemberManager.svelte';
 </script>
 
-<div>
-	<HomeManager />
+<div class="dashboard-wrapper">
+	<div class="home-section">
+		<HomeManager />
+	</div>
+	<div class="contact-us-section">
+		<ContactUsManager />
+	</div>
+	<div class="service-section">
+		<ServiceManager />
+	</div>
+	<div class="member-section">
+		<MemberManager />
+	</div>
+	<div class="article-section">
+		<ArticleManager />
+	</div>
 </div>
-<div>
-	<ContactUsManager />
-</div>
-<div>
-	<ServiceManager />
-</div>
-<div>
-	<MemberManager />
-</div>
-<div>
-	<ArticleManager />
-</div>
+
+
+<style lang="scss">
+  .dashboard-wrapper {
+    display: grid;
+    grid-template:
+	'home'
+	'contact-us'
+	'services'
+	'members'
+	'articles';
+    gap: 1rem;
+  }
+
+  .home-section {
+    grid-area: home;
+  }
+
+  .contact-us-section {
+    grid-area: contact-us;
+  }
+
+  .service-section {
+    grid-area: services;
+  }
+
+  .member-section {
+    grid-area: members;
+  }
+
+  .article-section {
+    grid-area: articles;
+  }
+
+  @media (min-width: 768px) {
+    .dashboard-wrapper {
+      grid-template: 'home contact-us'
+		'services services'
+		'members members'
+		'articles articles';
+    }
+  }
+</style>

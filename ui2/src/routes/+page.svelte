3602e2e3c722521ca0124f0d73c3b17a07d3080b
boris <boris.lok@cienet.com>
2025-01-02 09:35:08 +0800

    [Misc] An example of using tailwind css.

--- ui2/src/routes/+page.svelte
+++ ui2/src/routes/+page.svelte
@@ -1,13 +1,3 @@
-<script>
-	import "../app.css";
-</script>
-
 <h1 class="text-3xl font-bold underline">
 	Hello Tailwind!
 </h1>
-
-<style lang="postcss">
-	:global(html) {
-    background-color: theme(colors.gray.100);
-	}
-</style>

a55b0c0d396cfdc631583ee220cff0c3d5796403
boris <boris.lok.tw@outlook.com>
2025-01-09 16:42:35 +0800

    [Feature] Add login page's components.

---
+++ ui2/src/lib/components/common/Input.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	type InputProps = {
+		label: string,
+		name: string,
+		type: 'text' | 'password' | 'number',
+		value: string,
+		hasError: boolean,
+		onInput?: (event: Event & { currentTarget: EventTarget & HTMLInputElement }) => void,
+	};
+
+	let { label, name, type, value, hasError, onInput }: InputProps = $props();
+
+	function typeAction(node: HTMLInputElement) {
+		node.type = type;
+	}
+</script>
+
+<div class="mb-4">
+	<label class="block text-gray-700 text-sm font-bold mb-2" for={name}>{label}</label>
+	<input
+		class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:shadow-[0_0_0_3px_rgba(66,153,225,.5)] focus:outline-none"
+		class:error={hasError} id={name} {name} oninput={onInput} type="text" use:typeAction {value} />
+</div>
---
+++ ui2/src/routes/admin/login/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+	import Input from '$lib/components/common/Input.svelte';
+
+	type Data = {
+		username: string;
+		password: string;
+	}
+
+	let data: Data = {
+		username: '',
+		password: ''
+	};
+
+	function onDataChanged<K extends keyof Data>(key: K, e: Event & { currentTarget: EventTarget & HTMLInputElement }) {
+		if (!e.target) {
+			return;
+		}
+
+		const { value } = e.target as HTMLInputElement;
+		data = {
+			...data,
+			[key]: value
+		};
+	}
+</script>
+
+<div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+	<Input hasError={false} label="Username" name="username" onInput={e => onDataChanged('username', e)} type="text"
+				 value="" />
+	<Input hasError={false} label="Password" name="password" onInput={e => onDataChanged('password', e)} type="password"
+				 value="" />
+</div>

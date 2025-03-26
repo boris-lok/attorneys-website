a50e67f2e09049396b63e8911df4a5151d8b4ed6
boris <boris.lok@cienet.com>
2025-03-26 16:54:33 +0800

    [Feature] Change the style of home & services page.

--- ui2/src/routes/(app)/home/+page.svelte
+++ ui2/src/routes/(app)/home/+page.svelte
@@ -37,7 +37,7 @@
 <div class="relative flex md:flex-row flex-col md:items-center">
     <div class="overflow-clip hidden md:block">
         <img
-            class="w-full h-screen"
+            class="h-[48rem]"
             src="https://images.pexels.com/photos/139195/pexels-photo-139195.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
             alt="home-bg"
         />
--- ui2/src/routes/(app)/services/+page.svelte
+++ ui2/src/routes/(app)/services/+page.svelte
@@ -4,12 +4,14 @@
     import { finalize, tap } from 'rxjs'
     import type { ServiceData } from '$lib/types'
     import Markdown from '@magidoc/plugin-svelte-marked'
+    import IconifyIcon from '@iconify/svelte'
 
     let services: ServiceData[] = $state([])
     let isLoading = $state(false)
+    let selectedService = $state('')
 
-    function onServiceClicked() {
-
+    function onServiceClicked(id: string) {
+        selectedService = id
     }
 
     function fetchData() {
@@ -30,29 +32,38 @@
     $effect(() => fetchData())
 </script>
 
-<div class="relative flex flex-col">
-    <p class="text-4xl px-16 pt-16 text-[var(--primary-color)] font-bold">法律服務項目</p>
-    <div class="relative flex md:flex-row flex-col md:items-center mt-8">
-        <div class="flex-1">
-            <div class="px-16">
-                {#each services as service}
-                    <div class="overflow-clip rounded flex flex-col border border-green-700 my-4" onclick={() => onServiceClicked(service.id)}>
-                        <p class="px-8 py-4 text-lg font-bold text-[var(--primary-color)]">
-                            {service.data.title}
-                        </p>
+<div class="relative flex md:flex-row flex-col md:items-center">
+    <div class="relative flex flex-col flex-1">
+        <p class="text-4xl px-16 pt-16 text-[var(--primary-color)] font-bold">法律服務項目</p>
+        <div class="relative flex md:flex-row flex-col md:items-center mt-8">
+            <div class="flex-1">
+                <div class="px-16">
+                    {#each services as service}
+                        <button class="overflow-clip rounded flex flex-col border border-green-700 my-4 py-4 px-4 w-full text-left cursor-pointer" onclick={() => onServiceClicked(service.id)}>
+                            <div class="flex flex-row items-center justify-between group" class:show={selectedService == service.id}>
+                                <p class="text-lg font-bold text-[var(--primary-color)]">
+                                    {service.data.title}
+                                </p>
+                                <IconifyIcon icon="ooui:collapse" class="h-4 w-4 text-[var(--primary-color)] rotate-180 group-[.show]:rotate-0 md:transition-[rotate] duration-200"/>
+                            </div>
 
-                        <div class="prose h-0 overflow-hidden [&.show]:h-fit px-8">
-                            <Markdown source={service.data.data}></Markdown>
-                        </div>
-                    </div>
-                {/each}
+
+                            <div class="h-0 bg-green-800 w-0 mt-1 [&.show]:h-[1px] [&.show]:w-full [&.show]:my-4" class:show={selectedService == service.id}></div>
+
+                            <div class="prose max-h-0 overflow-hidden [&.show]:max-h-64 md:transition-[max-height] duration-300 ease-in-out" class:show={selectedService == service.id}>
+                                <Markdown source={service.data.data}></Markdown>
+                            </div>
+                        </button>
+                    {/each}
+                </div>
             </div>
         </div>
-
-        <div class="overflow-clip hidden md:block w-[50%] rounded-2xl">
-            <img
-                src="https://images.pexels.com/photos/4186912/pexels-photo-4186912.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt="home-bg" />
-        </div>
     </div>
+
+    <div class="overflow-clip hidden md:block flex-1 h-[48rem]">
+        <img
+            src="https://images.pexels.com/photos/4186912/pexels-photo-4186912.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="home-bg" />
+    </div>
+
 </div>

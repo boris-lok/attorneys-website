942ee1ccd1122fb71a1320ab5ce2ac70f11df300
boris <boris.lok@cienet.com>
2025-04-09 11:43:07 +0800

    [Fix] Background in small device layout.

--- ui/src/lib/components/Background.svelte
+++ ui/src/lib/components/Background.svelte
@@ -2,6 +2,26 @@
     import bgImage from '$lib/assets/bg-image.webp'
     import smBgImage from '$lib/assets/bg-image.sm.webp'
     import IconifyIcon from '@iconify/svelte'
+
+    type ContactUsType = {
+        icon: string
+        link: string
+    }
+
+    let contactUs: ContactUsType[] = [
+        {
+            icon: 'cib:line',
+            link: 'https://lin.ee/NW5iyIk'
+        },
+        {
+            icon: 'skill-icons:instagram',
+            link: 'https://www.instagram.com/chenwanglaw?igsh=MW0yY3pkbzlxN2lidQ%3D%3D&utm_source=qr'
+        },
+        {
+            icon: 'devicon:facebook',
+            link: 'https://www.facebook.com/share/1BfUcyRZki/?mibextid=wwXIfr&ref=xav_pl_fb_external_link_ios'
+        }
+    ]
 </script>
 
 <div class="relative flex h-36 justify-center overflow-clip md:h-[480px]">
@@ -15,31 +35,18 @@
     </picture>
 
     <div class="z-10 pt-4 md:pt-36 text-center">
-        <p class="text-6xl text-gray-300 md:text-6xl">勤睿國際法律事務所</p>
+        <p class="text-2xl md:text-4xl lg:text-6xl text-gray-300 md:text-6xl">勤睿國際法律事務所</p>
 
-        <div class="flex flex-col md:mt-8">
-
-            <div class="flex flex-col lg:flex-row gap-16">
-                <a class="flex flex-row gap-4 md:rounded md:border md:px-8 md:py-4 w-fit border-yellow-100"
-                   href="https://lin.ee/NW5iyIk"
-                   target="_blank">
-                    <IconifyIcon class="h-10 w-10 text-green-400" icon="cib:line" />
-                    <p class="text-yellow-100 text-xl md:text-3xl">諮詢</p>
-                </a>
-
-                <a class="flex flex-row gap-4 md:rounded md:border md:px-8 md:py-4 w-fit border-yellow-100"
-                   href="https://www.instagram.com/chenwanglaw?igsh=MW0yY3pkbzlxN2lidQ%3D%3D&utm_source=qr"
-                   target="_blank">
-                    <IconifyIcon class="h-10 w-10 text-green-400" icon="skill-icons:instagram" />
-                    <p class="text-yellow-100 text-xl md:text-3xl">諮詢</p>
-                </a>
-
-                <a class="flex flex-row gap-4 md:rounded md:border md:px-8 md:py-4 w-fit border-yellow-100"
-                   href="https://www.facebook.com/share/1BfUcyRZki/?mibextid=wwXIfr&ref=xav_pl_fb_external_link_ios"
-                   target="_blank">
-                    <IconifyIcon class="h-10 w-10 text-green-400" icon="devicon:facebook" />
-                    <p class="text-yellow-100 text-xl md:text-3xl">諮詢</p>
-                </a>
+        <div class="fixed lg:relative flex flex-col md:mt-8 bottom-[3rem] lg:bottom-auto right-[1.5rem] lg:right-auto">
+            <div class="flex flex-col lg:flex-row gap-6 lg:gap-16">
+                {#each contactUs as item (item.link)}
+                    <a class="flex flex-row gap-4 lg:rounded lg:border lg:px-8 lg:py-4 w-fit border-yellow-100 items-center rounded-[50%] p-2 bg-white lg:bg-transparent shadow-xl lg:shadow-none"
+                       href={item.link}
+                       target="_blank">
+                        <IconifyIcon class="h-8 w-8 lg:h-10 lg:w-10 text-green-400" icon={item.icon} />
+                        <p class="hidden lg:block lg:text-yellow-100 lg:text-xl lg:text-3xl">諮詢</p>
+                    </a>
+                {/each}
             </div>
         </div>
     </div>

413964545dac271afd62222d0c86fa89d64f353c
boris <boris.lok@cienet.com>
2025-04-11 14:44:34 +0800

    [Feature] Add `create` button in `category` page.

--- ui/src/routes/admin/(dashboard)/categories/+page.svelte
+++ ui/src/routes/admin/(dashboard)/categories/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 
-    import type { CategoryData } from '$lib/types'
+    import type { CategoryData, Language } from '$lib/types'
     import { startWithTap } from '$lib/utils'
     import { finalize, tap } from 'rxjs'
     import { CategoryService } from '$lib/services/category.service'
     import Loading from '$lib/components/common/Loading.svelte'
+    import Icon from '@iconify/svelte'
 
     let categories: CategoryData[] = $state([])
     let isLoading = $state(false)
-    let language = 'zh'
+    let language: Language = 'zh'
 
     function fetchData() {
-        CategoryService.list('zh')
+        CategoryService.list(language)
             .pipe(
                 startWithTap(() => (isLoading = true)),
                 finalize(() => (isLoading = false)),
@@ -28,6 +29,11 @@
 </script>
 
 <Loading show={isLoading} />
+<div class="relative my-4 flex flex-row justify-end px-2">
+    <a href="/admin/categories/edit">
+        <Icon icon="gridicons:create" width="24" height="24" />
+    </a>
+</div>
 <div>
     {#each categories as category (category.id)}
         <div>

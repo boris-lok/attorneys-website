880057d7c9c7eba430094279635edb48dbde7733
boris <boris.lok@cienet.com>
2025-04-11 17:12:38 +0800

    [Fix] Can't scroll.

--- ui/src/lib/components/common/Loading.svelte
+++ ui/src/lib/components/common/Loading.svelte
@@ -8,10 +8,12 @@
     let { show }: InputProps = $props()
 
 
+    $effect(() => {
+        document.body.style.overflow = show ? 'hidden' : ''
+    })
 </script>
 
-<svelte:window on:wheel|nonpassive|preventDefault />
-<div class="w-full h-screen hidden top-0 left-0 bg-gray-100/50 [.show]:flex fixed items-center justify-center"
+<div class="w-full h-screen hidden top-0 left-0 bg-gray-100/50 [.show]:flex fixed items-center justify-center z-50"
      class:show>
     <IconifyIcon class="w-12 h-12" icon="eos-icons:bubble-loading" />
 </div>

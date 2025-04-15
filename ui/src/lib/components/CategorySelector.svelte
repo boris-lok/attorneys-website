c0329e350560b4db5b8531201f89397fd9704b9e
boris <boris.lok@cienet.com>
2025-04-15 15:42:29 +0800

    [Refactoring] fix <p> inside a button.

--- ui/src/lib/components/CategorySelector.svelte
+++ ui/src/lib/components/CategorySelector.svelte
@@ -19,8 +19,8 @@
 </script>
 
 <div class="flex flex-col gap-2 px-8">
-    <button class="cursor-pointer" onclick={() => isOpen = !isOpen}>
-        <p class="text-xl font-bold text-left w-36">類別</p>
+    <button class="cursor-pointer text-xl font-bold text-left w-36" onclick={() => isOpen = !isOpen}>
+        類別
     </button>
     <div
         class="flex flex-col h-0 overflow-y-hidden [.active]:h-48 [.active]:overflow-y-scroll transition-[height] duration-200 lg:h-fit lg:overflow-y-visible"

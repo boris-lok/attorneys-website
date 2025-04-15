cdd4dcb20ebf368721577eedac0a16d4f7f9cb9b
boris <boris.lok.tw@outlook.com>
2025-04-15 23:02:47 +0800

    [Feature] Improve the admin member page layout.

--- ui/src/routes/admin/(dashboard)/members/+page.svelte
+++ ui/src/routes/admin/(dashboard)/members/+page.svelte
@@ -35,24 +35,27 @@
                 <Icon height="24" icon="gridicons:create" width="24" />
             </a>
         </div>
-        <div class="relative flex flex-col gap-8 md:flex-row">
+        <div
+            class="relative flex flex-col gap-8 px-4 md:flex-row mt-4 md:justify-center"
+        >
             {#each members as member}
                 <div
-                    class="flex w-fit flex-row items-center gap-2 rounded px-4 py-4 shadow"
+                    class="flex h-36 w-full flex-row items-center gap-6 rounded px-4 py-4 shadow-md md:w-84 lg:w-96"
                 >
                     {#if member.avatar}
-                        <Image
-                            alt={member.name}
-                            image={member.avatar}
-                            size={48}
-                        />
+                        <div class="h-24 w-24">
+                            <Image
+                                alt={member.name}
+                                image={member.avatar}
+                            />
+                        </div>
                     {:else}
                         <IconifyIcon
                             icon="radix-icons:avatar"
-                            class="h-12 w-12"
+                            class="h-24 w-24"
                         />
                     {/if}
-                    <p class="text-lg">{member.name}</p>
+                    <p class="text-2xl">{member.name}</p>
                     <div class="py-2 pl-12">
                         <a href="/admin/members/edit/{member.id}">
                             <Icon

a75e3560b258db2e5aeb366051c07187f3b06902
boris <boris.lok@cienet.com>
2025-10-30 09:20:48 +0800

    [Feature] Update the script and config.

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+set -e
 
 echo "Renew certification..."
 
-CMD="docker run -it --rm -v /home/attorneys-website/data/certbot/conf:/etc/letsencrypt -v /home/attorneys-website/data/certbot/www:/var/www/certbot certbot/certbot renew"
-
-RELOAD_NGINX_CMD="docker exec nginx nginx -s reload"
-
 # renew the cert
 echo "Execute the renew command..."
-$CMD
+docker run --rm \
+	-v /home/attorneys-website/data/certbot/conf:/etc/letsencrypt \
+	-v /home/attorneys-website/data/certbot/www:/var/www/certbot \
+	certbot/certbot renew --quiet
+
 
 # reload the nginx to get the new cert
-echo "Reload the nginx..."
-$RELOAD_NGINX_CMD
+echo "Reload nginx..."
+docker exec nginx nginx -s reload

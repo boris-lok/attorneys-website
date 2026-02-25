dd8b03bb19b8599e026dd35efc794ce36888c6f1
boris <boris.lok@cienet.com>
2026-02-25 09:17:21 +0800

    [Fix] modify the type of ID in roles table, and drop the `work_log_status`.

--- backend/migrations/20260224010128_create_users_roles.up.sql
+++ backend/migrations/20260224010128_create_users_roles.up.sql
@@ -2,13 +2,16 @@
 
 create table roles
 (
-    id   uuid primary key default gen_random_uuid(),
+    id   smallserial primary key,
     name text not null unique
 );
 
 create table user_roles
 (
     user_id uuid not null references users (id),
-    role_id uuid not null references roles (id),
+    role_id smallserial not null references roles (id),
     primary key (user_id, role_id)
 );
+
+insert into roles (name) values ('Admin');
+insert into roles (name) values ('Lawyer');
--- backend/migrations/20260225005329_add_work_log_status.down.sql
+++ backend/migrations/20260225005329_add_work_log_status.down.sql
@@ -1,3 +1,4 @@
 -- Add down migration script here
 
 alter table work_logs drop column status;
+drop type work_log_status;

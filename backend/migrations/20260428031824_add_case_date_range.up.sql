55529e8990734f11954bc5db320de53dd252a258
boris <boris.lok@cienet.com>
2026-04-28 15:13:58 +0800

    [Fix] SQL error.

--- backend/migrations/20260428031824_add_case_date_range.up.sql
+++ backend/migrations/20260428031824_add_case_date_range.up.sql
@@ -1,4 +1,4 @@
 -- Add up migration script here
 
-alter table cases add column started_at timestampz;
-alter table cases add column ended_at timestampz;
+alter table cases add column started_at timestamptz;
+alter table cases add column ended_at timestamptz;

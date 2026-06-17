973f2d7908fb62018e502c4dccea350c44d8589f
boris <boris.lok@cienet.com>
2026-06-17 16:00:10 +0800

    [Refactoring] PendingWorkLog component. - remove un-necessary $state - disable the button when a user clicked the *accept* or *reject*. - move loading to locally.

--- ui/src/routes/b/(dashboard)/case/PendingWorkLog.svelte
+++ ui/src/routes/b/(dashboard)/case/PendingWorkLog.svelte
@@ -2,7 +2,6 @@
     import { WorkLogServices } from '$lib/services/workLog.service'
     import { dateRangeFormatter, roundTo } from '$lib/utils'
     import IconifyIcon from '@iconify/svelte'
-    import Loading from '$lib/components/shared/Loading.svelte'
     import type { PendingWorkLog } from '$lib/types'
 
     type Props = PendingWorkLog & {
@@ -10,17 +9,17 @@
     }
 
     let { onDone, ...rest }: Props = $props()
-    // svelte-ignore state_referenced_locally
-    let copiedData: PendingWorkLog = $state({ ...rest })
-    const hrs = roundTo(copiedData.duration / 60, 2)
+    const hrs = $derived(roundTo(rest.duration / 60, 2))
     let isLoading = $state(false)
 
     async function onClicked(status: 'accepted' | 'rejected') {
+        if (isLoading) return
+
         isLoading = true
         if (status === 'accepted') {
-            await WorkLogServices.accept(copiedData.id)
+            await WorkLogServices.accept(rest.id)
         } else {
-            await WorkLogServices.reject(copiedData.id)
+            await WorkLogServices.reject(rest.id)
         }
         isLoading = false
 
@@ -28,19 +27,15 @@
     }
 </script>
 
-{#if isLoading}
-    <Loading />
-{/if}
-
 <div
     class="m-4 rounded p-4 shadow md:m-0 md:flex md:min-h-12 md:flex-row md:items-center md:gap-4 md:rounded-none md:p-2 md:shadow-none md:hover:bg-gray-50"
 >
     <div class="my-2 flex-5/12 font-semibold text-nowrap md:my-0 md:font-medium">
-        {copiedData.description}
+        {rest.description}
     </div>
 
     <div class="my-1 flex-4/12 text-sm text-gray-500 md:my-0 md:text-gray-700">
-        <p>{dateRangeFormatter(copiedData.startedAt, copiedData.endedAt)}</p>
+        <p>{dateRangeFormatter(rest.startedAt, rest.endedAt)}</p>
     </div>
 
     <div class="flex-1/12 text-sm">
@@ -49,15 +44,21 @@
     </div>
 
     <div class="flex h-fit flex-2/12 flex-row justify-end gap-2">
-        <button class="cursor-pointer" onclick={() => onClicked('accepted')}>
-            <IconifyIcon
-                class="h-4 w-4 text-green-500 md:h-6 md:w-6"
-                icon="line-md:circle-to-confirm-circle-transition"
-            />
-        </button>
+        {#if isLoading}
+            <div>
+                <IconifyIcon class="text-blue-500 h-6 w-6 mr-6" icon="svg-spinners:90-ring-with-bg" />
+            </div>
+        {:else}
+            <button class="cursor-pointer" onclick={() => onClicked('accepted')} aria-label="Accept work log">
+                <IconifyIcon
+                    class="h-4 w-4 text-green-500 md:h-6 md:w-6"
+                    icon="line-md:circle-to-confirm-circle-transition"
+                />
+            </button>
 
-        <button class="cursor-pointer" onclick={() => onClicked('rejected')}>
-            <IconifyIcon class="h-4 w-4 text-red-500 md:h-6 md:w-6" icon="line-md:close-circle" />
-        </button>
+            <button class="cursor-pointer" onclick={() => onClicked('rejected')} aria-label="Reject work log">
+                <IconifyIcon class="h-4 w-4 text-red-500 md:h-6 md:w-6" icon="line-md:close-circle" />
+            </button>
+        {/if}
     </div>
 </div>
